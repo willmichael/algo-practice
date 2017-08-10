60424871616d5a641de2b4b57b051bc34e9e467b
willmichael <oleemir@gmail.com>
2017-08-10 16:07:35 -0700

    recursion method for regex_matching, but exceeds time limit

--- leet/regex_matching.py
+++ leet/regex_matching.py
@@ -5,43 +5,33 @@ class Solution(object):
         :type p: str
         :rtype: bool
         """
-        s_idx = 0
-        p_idx = 0
-        prev_char = ''
-        repeat = 0
+        if s == '' and p == '':
+            return True
+        elif p == '' and s!= '':
+            return False
+        elif s == '' and p != '':
+            if len(p) > 1 and p[1] == '*':
+                return self.isMatch(s, p[2:])
+            else:
+                return False
+        elif s[0] == p[0]:
+            if len(p) > 1 and p[1] == '*':
+                return self.isMatch(s[1:], p) or self.isMatch(s, p[2:])
+            else:
+                return self.isMatch(s[1:], p[1:])
+        elif p[0] == '.':
+            if len(p) > 1 and p[1] == '*':
+                return self.isMatch(s[1:], p) or self.isMatch(s, p[2:])
+            else:
+                return self.isMatch(s[1:], p[1:])
+        else:
+            return self.isMatch(s, p[1:])
 
-        for c in s:
-            while True:
-                if p_idx > len(p)-1:
-                    return False
-                elif c == p[p_idx]:
-                    prev_char = p[p_idx]
-                    p_idx += 1
-                elif p_idx+1 < len(p)-1 and p[p_idx+1] == '*' and p[p_idx] != '.':
-                    p_idx += 2
-                    continue
-                else:
-                    if p[p_idx] == '.':
-                        prev_char = p[p_idx]
-                        p_idx += 1
-                    elif p[p_idx] == '*':
-                        if c == prev_char or prev_char == '.':
-                            pass
-                        elif p_idx+1 <= len(p)-1 and p[p_idx+1] == c:
-                            p_idx += 2
-                        else:
-                            return False
-                    else:
-                        return False
-                s_idx += 1
-                break
-        if p_idx != len(p):
-            return False
-        if s_idx != len(s):
-            return False
-        return True
-                    
+        
+
+
+
 s = Solution()
-pat = 'a*'
-query = 'aa'
+pat = 'a*a*a*a*a*a*a*a*a*a*c'
+query = 'aaaaaaaaaaaaab'
 print s.isMatch(query, pat)
