bc97af60b779df4169fef31c673b9792ecc49549
willmichael <oleemir@gmail.com>
2017-08-10 17:04:34 -0700

    clone graph recursion implementation exceeds max depth

---
+++ leet/clone_graph.py
@@ -0,0 +1,21 @@
+# Definition for a undirected graph node
+# class UndirectedGraphNode:
+#     def __init__(self, x):
+#         self.label = x
+#         self.neighbors = []
+
+class Solution:
+    # @param node, a undirected graph node
+    # @return a undirected graph node
+    def cloneGraph(self, node):
+        if node == None:
+            return None
+        elif not node.neighbors:
+            return UndirectedGraphNode(node.label)
+        else:
+            new_node = UndirectedGraphNode(node.label)
+            for nb in node.neighbors:
+                new_node.neighbors.append(self.cloneGraph(nb))
+            return new_node
+
+        
