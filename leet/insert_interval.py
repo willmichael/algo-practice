b5e097bf2ce5ec203364606a9175af644e8dbf44
willmichael <oleemir@gmail.com>
2017-08-27 18:53:45 -0700

    insert_interval

---
+++ leet/insert_interval.py
@@ -0,0 +1,43 @@
+# Definition for an interval.
+# class Interval(object):
+#     def __init__(self, s=0, e=0):
+#         self.start = s
+#         self.end = e
+
+class Solution(object):
+    def insert(self, intervals, newInterval):
+        i = 0
+        while i <= len(intervals)-1:
+            if i == len(intervals):
+                print 'hihi'
+                intervals.insert(i, newInterval)
+                break
+            elif intervals[i].start > newInterval.start:
+                intervals.insert(i, newInterval)
+                break
+            i += 1
+        # if len(intervals) < 1:
+            # return intervals
+        print len(intervals)
+        print i
+        i = 0
+        while i < len(intervals)-1:
+            if len(intervals) < 2:
+                return intervals
+            elif intervals[i].start == intervals[i+1].start:
+                # Grab larger interval range
+                if intervals[i].end < intervals[i+1].end:
+                    intervals.pop(i)
+                else:
+                    intervals.pop(i+1)
+            elif intervals[i].end >= intervals[i+1].start:
+                # Merge intervals
+                if intervals[i].end > intervals[i+1].end:
+                    intervals.pop(i+1)
+                else:
+                    intervals.insert(i, Interval(intervals[i].start, intervals[i+1].end))
+                    intervals.pop(i+1)
+                    intervals.pop(i+1)
+            else:
+                i += 1
+        return intervals
